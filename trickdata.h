3193467091189a0f54482096f31bb6beb7c2c5bb
eweitnauer <eweitnauer@gmail.com>
2010-08-13 22:16:51 +0200

    converted sound files to wav format. sounds are played and trick list is updated on freestyle game screen.

--- trickdata.h
+++ trickdata.h
@@ -3,6 +3,7 @@
 
 #include <QString>
 #include <QMap>
+#include <QStringList>
 
 
 class TrickData
@@ -10,13 +11,18 @@ class TrickData
 public:
     TrickData();
     static TrickData* instance();
-
+    static bool hasTrick(QString trickId);
     static int getPoints(QString trickid);
-    static QString getSoundByName(QString trickid, bool praise_if_unknown);
+    static QString getSoundForTrick(QString trickid, bool praise_if_unknown=true);
     static QString getPraiseSound();
+    static QString getSoundForLevel(int i);
+    static QString pickOne(QStringList list);
 
 private:
     QMap<QString, int> points_map;
+    QMap<QString, QStringList> sounds_map;
+    QStringList sounds_good;
+    QString sound_path;
 };
 
 
